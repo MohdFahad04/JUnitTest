d22490ea9bb77e6bb911dd964554b00e33ac29cd
HP <HP@10.12.21.134>
2022-11-21 17:16:07 +0530

    junittest

---
+++ src/jUnitTest2/jUnitFunctions2.java
@@ -0,0 +1,16 @@
+package jUnitTest2;
+
+//import static org.junit.Assert.*;
+
+//import org.junit.Test;
+
+public class jUnitFunctions2 {
+
+	public int addNumbers(int num1, int num2) {
+		return num1 + num2;
+	}
+	public String addStrings(String s1, String s2) {
+		return s1+s2;
+	}
+
+}
